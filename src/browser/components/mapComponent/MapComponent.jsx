84a80dd59e2f16ce7312c35e5d7370757df5a68d
Giancarlo Gerbaz <giancarlo.gerbaz@casa.it>
2017-08-23 18:43:04 +0200

    Map - InfoWindow for cluster items +  Article list style & actions

--- src/browser/components/mapComponent/MapComponent.jsx
+++ src/browser/components/mapComponent/MapComponent.jsx
@@ -5,6 +5,7 @@ import {
   withGoogleMap,
   GoogleMap,
   Marker,
+  InfoWindow,
 } from 'react-google-maps';
 import { default as MarkerClusterer } from 'react-google-maps/lib/addons/MarkerClusterer';
 import { default as InfoBox } from 'react-google-maps/lib/addons/InfoBox';
@@ -23,7 +24,7 @@ const GettingStartedGoogleMap = withGoogleMap(props => (
     <MarkerClusterer
       zoomOnClick={false}
       gridSize={1}
-      onClick={props.onClusterClick}
+      onClick={(e) => { props.onClusterClick(e); }}
     >
       {props.markers.map((marker, index) => (
         <Marker
@@ -39,10 +40,27 @@ const GettingStartedGoogleMap = withGoogleMap(props => (
         <InfoBox
           position={props.infoBoxData.markerPosition}
           onCloseClick={() => { props.onCloseClick(); }}
-          options={{ closeBoxMargin: '10px', disableAutoPan: true, boxClass: props.infoBoxData.boxClass }}
+          options={{
+            closeBoxMargin: '10px',
+            disableAutoPan: true,
+            boxClass: props.infoBoxData.boxClass,
+          }}
         >
           {props.infoBoxData.content}
         </InfoBox>
+    }
+    {
+      props.infoWindowData !== null &&
+        <InfoWindow
+          onDomReady={() => { props.iwDOMReady(); }}
+          position={props.infoWindowData.markerPosition}
+          onCloseClick={() => { props.onCloseClick(); }}
+          options={{
+            disableAutoPan: true,
+          }}
+        >
+          {props.infoWindowData.content}
+        </InfoWindow>
     }
   </GoogleMap>
 ));
@@ -138,12 +156,19 @@ class MapComponent extends React.Component {
     super(props);
     this.state = {
       markers: [],
+      articles: [],
       infoBoxData: null,
+      infoWindowData: null,
     };
+
+    this.validItems = [];
+
     this.handleMarkerClick = this.handleMarkerClick.bind(this);
     this.handleCloseClick = this.handleCloseClick.bind(this);
     this.onClusterClick = this.onClusterClick.bind(this);
     this.handleMapMounted = this.handleMapMounted.bind(this);
+    this.iwDOMReady = this.iwDOMReady.bind(this);
+    this.selectMarkerById = this.selectMarkerById.bind(this);
   }
   componentWillMount() {
     const { fetchMap } = this.props;
@@ -154,7 +179,7 @@ class MapComponent extends React.Component {
     if (!newProps.isFetching) {
       if (newProps.items && newProps.items.length > 0) {
         const items = newProps.items;
-        const validItems = items && items.length > 0 ?
+        this.validItems = items && items.length > 0 ?
           items.filter(
             (el) => {
               if (el.property.address.location) {
@@ -166,8 +191,8 @@ class MapComponent extends React.Component {
         :
           [];
 
-        const box = createBBox(validItems, newProps.viewport.width, newProps.viewport.height);
-        const markers = validItems.map(
+        const box = createBBox(this.validItems, newProps.viewport.width, newProps.viewport.height);
+        const markers = this.validItems.map(
           el => (
             {
               title: el.id,
@@ -203,9 +228,39 @@ class MapComponent extends React.Component {
             }
           ),
         );
+        const articles = this.validItems.map(
+          (el, index) => {
+            return (
+              <li key={`map-article-${index}`} onClick={() => { this.selectMarkerById(el.id); }}>
+                <img className="pic" src={`${el.property.media._meta.server}/360x265${el.property.media.gallery[0].uri}`} alt={el.property.address.streetAddress} />
+                <h1><a href="/">{el.property.address.streetAddress}</a></h1>
+                <h2>{el.price.display}</h2>
+                <p className="infos">
+                  {
+                    el.property.features.general.landsize &&
+                    <span>
+                      {el.property.features.general.landsize.value}{' '}
+                      <span className="label">mq</span>
+                    </span>
+                  }
+                  {
+                    el.property.features.general.rooms &&
+                      <span>
+                        {' '}|{' '}
+                        {el.property.features.general.rooms.value}{' '}
+                        <span className="label">locali</span>
+                      </span>
+                  }
+                </p>
+              </li>
+            );
+          },
+        );
+
         this.setState({
           box,
           markers,
+          articles,
         });
       }
     }
@@ -214,18 +269,18 @@ class MapComponent extends React.Component {
   handleMarkerClick(event, targetMarker) {
     const mapElementSize = document.querySelector('#map_holder').getBoundingClientRect();
     const markerPxPosition = fromLatLngToPixel(this._map, event.latLng);
-    const infoBoxCorrectPxPosition = { x: markerPxPosition.x - 195, y: markerPxPosition.y - 395 };
+    const infoBoxCorrectPxPosition = { x: markerPxPosition.x - 195, y: markerPxPosition.y - 385 };
 
     let boxClass = 'infoBox';
 
     if (markerPxPosition.x + 195 > mapElementSize.right) {
-      infoBoxCorrectPxPosition.x = markerPxPosition.x - 410;
+      infoBoxCorrectPxPosition.x = markerPxPosition.x - 397;
       infoBoxCorrectPxPosition.y = markerPxPosition.y - 203;
       boxClass = 'infoBox arrow-right';
     }
 
     if (markerPxPosition.x - 195 < 0) {
-      infoBoxCorrectPxPosition.x = markerPxPosition.x + 30;
+      infoBoxCorrectPxPosition.x = markerPxPosition.x + 13;
       infoBoxCorrectPxPosition.y = markerPxPosition.y - 203;
       boxClass = 'infoBox arrow-left';
     }
@@ -238,32 +293,153 @@ class MapComponent extends React.Component {
         infoBoxCorrectPxPosition.x = markerPxPosition.x + 15;
         infoBoxCorrectPxPosition.y = markerPxPosition.y - 15;
       } else {
-        infoBoxCorrectPxPosition.y = markerPxPosition.y + 20;
+        infoBoxCorrectPxPosition.y = markerPxPosition.y - 10;
         boxClass = 'infoBox arrow-up';
       }
     }
 
     const coords = fromPixelToLatLng(this._map, { x: infoBoxCorrectPxPosition.x, y: infoBoxCorrectPxPosition.y });
-    
     const markerPosition = new google.maps.LatLng({ lat: coords.lat(), lng: coords.lng() });
-    
-    this.setState({ infoBoxData: { content: targetMarker.infoContent, markerPosition, boxClass } });
+    this.setState({ infoBoxData: { content: targetMarker.infoContent, markerPosition, boxClass }, infoWindowData: null });
   }
 
   handleCloseClick() {
-    this.setState({ infoBoxData: null });
+    this.setState({ infoBoxData: null, infoWindowData: null });
   }
 
   onClusterClick(cluster) {
+    const mapElementSize = document.querySelector('#map_holder').getBoundingClientRect();
+    const markerPxPosition = fromLatLngToPixel(this._map, cluster.getCenter());
+    const infoBoxCorrectPxPosition = { x: markerPxPosition.x, y: markerPxPosition.y };
+
+    let boxClass = 'infoBox';
+    /*
+    if (markerPxPosition.x + 195 > mapElementSize.right) {
+      infoBoxCorrectPxPosition.x = markerPxPosition.x - 397;
+      infoBoxCorrectPxPosition.y = markerPxPosition.y - 203;
+      boxClass = 'infoBox arrow-right';
+    }
+
+    if (markerPxPosition.x - 195 < 0) {
+      infoBoxCorrectPxPosition.x = markerPxPosition.x + 13;
+      infoBoxCorrectPxPosition.y = markerPxPosition.y - 203;
+      boxClass = 'infoBox arrow-left';
+    }
+*/
+    if (markerPxPosition.y - 395 < 0) {
+      if (boxClass.indexOf('right') > -1) {
+        infoBoxCorrectPxPosition.x = markerPxPosition.x - 395;
+        infoBoxCorrectPxPosition.y = markerPxPosition.y - 15;
+      } else if (boxClass.indexOf('left') > -1) {
+        infoBoxCorrectPxPosition.x = markerPxPosition.x + 15;
+        infoBoxCorrectPxPosition.y = markerPxPosition.y - 15;
+      } else {
+        infoBoxCorrectPxPosition.y = markerPxPosition.y + 421;
+        boxClass = 'infoBox arrow-up';
+      }
+    }
+
     const markers = cluster.getMarkers();
+    const clusterItems = [];
     markers.forEach(
-      marker => console.log(marker.title),
+      marker => clusterItems.push(marker.title),
     );
+    let count = 0;
+    const listItems = this.validItems.map(
+      (el) => {
+        if (clusterItems.indexOf(el.id) > -1) {
+          count += 1;
+          return (
+            <li key={`cluster-item-${count}`}>
+              <img className="pic" src={`${el.property.media._meta.server}/360x265${el.property.media.gallery[0].uri}`} alt={el.property.address.streetAddress} />
+              <h1><a href="/">{el.property.address.streetAddress}</a></h1>
+              <h2>{el.price.display}</h2>
+              <p className="infos">
+                {
+                  el.property.features.general.landsize &&
+                  <span>
+                    {el.property.features.general.landsize.value}{' '}
+                    <span className="label">mq</span>
+                  </span>
+                }
+                {
+                  el.property.features.general.rooms &&
+                    <span>
+                      {' '}|{' '}
+                      {el.property.features.general.rooms.value}{' '}
+                      <span className="label">locali</span>
+                    </span>
+                }
+              </p>
+            </li>
+          );
+        }
+        return null;
+      },
+    );
+    const iwHeight = count < 4 ? (count * 92) + 26 : (3 * 92) + 41;
+    const content = (
+      <div className="casaitem window" style={{ height: `${iwHeight}px` }}>
+        <div className="scrollingContCont">
+          <div className="scrollingCont">
+            <ul>
+              {listItems}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+    const coords = fromPixelToLatLng(this._map, { x: infoBoxCorrectPxPosition.x, y: infoBoxCorrectPxPosition.y });
+    const markerPosition = new google.maps.LatLng({ lat: coords.lat(), lng: coords.lng() });
+    this.setState({ infoWindowData: { content, markerPosition, boxClass }, infoBoxData: null });
   }
 
   handleMapMounted(map) {
     this._map = map;
   }
+
+  iwDOMReady() {
+    document.querySelector('.gm-style-iw').parentNode.className = this.state.infoWindowData.boxClass;
+  }
+
+  /*
+
+  TODO:
+    click on list item opens correct infobox
+*/
+  selectMarkerById(id) {
+    const marker = this.state.markers.filter(
+      el => el.title === id,
+    );
+    console.log(marker[0]);
+    const boxClass = 'infoBox';
+    const myLatlng = new google.maps.LatLng(marker[0].position.lat,marker[0].position.lng);
+    const markerPxPosition = fromLatLngToPixel(this._map, myLatlng);
+    const infoBoxCorrectPxPosition = { x: markerPxPosition.x - 195, y: markerPxPosition.y - 385 };
+    const coords = fromPixelToLatLng(this._map, { x: infoBoxCorrectPxPosition.x, y: infoBoxCorrectPxPosition.y });
+    const markerPosition = new google.maps.LatLng({ lat: coords.lat(), lng: coords.lng() });    
+    this.setState({ infoBoxData: { content: marker[0].infoContent, markerPosition, boxClass }, infoWindowData: null });
+    //this.setState({ infoBoxData: { content: marker[0].infoContent, markerPosition, boxClass }, infoWindowData: null });
+    /*
+    _fromUser = false;
+
+    const { list } = this.props;
+    const itemIndex = findIndexByKeyValue(list, 'id', id);
+
+    const { latitude, longitude } = list[itemIndex].address.location;
+
+    // this.map.props.map.setZoom(21);
+    // this.map.props.map.panTo(new google.maps.LatLng(latitude + 0.00011, longitude));
+
+    const { markers } = this.state;
+    const markerIndex = findIndexByKeyValue(markers, 'id', id);
+    const marker = markers[markerIndex];
+
+    this.setState({ infoShow: true, infoData: [marker], infoCenter: new google.maps.LatLng(marker.position.lat, marker.position.lng) });
+    
+    */
+  }
+  
 
   render() {
     const {
@@ -284,26 +460,33 @@ class MapComponent extends React.Component {
         modalType={modalType}
         toggleSiteHiddenComponents={toggleSiteHiddenComponents}
       >
-        <div id="map_holder" style={{ width: '100vw', height: '100vh' }}>
-          {
-            !this.props.isFetching &&
-              <GettingStartedGoogleMap
-                onMapMounted={this.handleMapMounted}
-                containerElement={
-                  <div style={{ height: '100%' }} />
-                }
-                mapElement={
-                  <div style={{ height: '100%' }} />
-                }
-                coords={{ lat: this.state.box.lat, lng: this.state.box.lon }}
-                zoom={this.state.box.zoom}
-                markers={this.state.markers}
-                onMarkerClick={this.handleMarkerClick}
-                onCloseClick={this.handleCloseClick}
-                onClusterClick={this.onClusterClick}
-                infoBoxData={this.state.infoBoxData}
-              />
-          }
+        <div className="casamap">
+          <div id="list_holder">
+            <ul>{this.state.articles}</ul>
+          </div>
+          <div id="map_holder" style={{ width: 'calc(100vw - 300px)', height: '100vh' }}>
+            {
+              !this.props.isFetching &&
+                <GettingStartedGoogleMap
+                  onMapMounted={this.handleMapMounted}
+                  containerElement={
+                    <div style={{ height: '100%' }} />
+                  }
+                  mapElement={
+                    <div style={{ height: '100%' }} />
+                  }
+                  coords={{ lat: this.state.box.lat, lng: this.state.box.lon }}
+                  zoom={this.state.box.zoom}
+                  markers={this.state.markers}
+                  onMarkerClick={this.handleMarkerClick}
+                  onCloseClick={this.handleCloseClick}
+                  onClusterClick={this.onClusterClick}
+                  infoBoxData={this.state.infoBoxData}
+                  infoWindowData={this.state.infoWindowData}
+                  iwDOMReady={this.iwDOMReady}
+                />
+            }
+          </div>
         </div>
       </Page>
     );
